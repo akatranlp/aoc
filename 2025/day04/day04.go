dc2b7edf812325137912c50086ccae8dec6817d0
Fabian Petersen <fabian@nf-petersen.de>
2025-12-04 00:49:53 +0100

    feat: 2025/day04 init

---
+++ 2025/day04/day04.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"aoc/lib/aoc"
+	"fmt"
+	"io"
+)
+
+type Day04 struct{}
+
+var _ aoc.Problem = (*Day04)(nil)
+
+func (*Day04) Part1(r io.Reader) int {
+	fmt.Println("Part1 not implemented")
+	return -1
+}
+
+func (*Day04) Part2(r io.Reader) int {
+	fmt.Println("Part2 not implemented")
+	return -1
+}
---
+++ 2025/day04/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"aoc/lib/aoc"
+)
+
+func main() {
+	day04 := &Day04{}
+	aoc.Run("2025/day04/input.txt", day04)
+}
